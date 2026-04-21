e40c4cd3fa9b221ae7172376bb25e320d6e7177e
Elshan Aliyev <aliyev@Elshans-MacBook-Pro.local>
2026-04-21 20:43:26 +0400

    first commit

---
+++ src/main/java/com/example/aliyevgrocery/AliyevGroceryApplication.java
@@ -0,0 +1,13 @@
+package com.example.aliyevgrocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliyevGroceryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliyevGroceryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aliyevgrocery/AliyevGroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aliyevgrocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliyevGroceryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
