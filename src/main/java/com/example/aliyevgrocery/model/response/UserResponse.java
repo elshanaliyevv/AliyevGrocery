3898d62f96fadfe93cc2de39307b41b36c36a069
Elshan Aliyev <aliyev@Elshans-MacBook-Pro.local>
2026-04-23 22:59:48 +0400

    UserResponse class added

---
+++ src/main/java/com/example/aliyevgrocery/model/response/UserResponse.java
@@ -0,0 +1,11 @@
+package com.example.aliyevgrocery.model.response;
+
+import lombok.Data;
+
+@Data
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String email;
+    private String number;
+}
