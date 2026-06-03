9364de468dc745adfbb2b30d05de4b8968032bcb
Elshan Aliyev <aliyev@Elshans-MacBook-Pro.local>
2026-06-03 16:46:03 +0400

    security filterchain updated

--- src/main/java/com/example/aliyevgrocery/config/SecurityConfig.java
+++ src/main/java/com/example/aliyevgrocery/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
