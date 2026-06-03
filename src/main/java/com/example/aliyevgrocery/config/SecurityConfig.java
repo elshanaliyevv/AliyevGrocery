cbb4627bcc19bfe6914ba5ccd461ee8e263a81c3
Elshan Aliyev <aliyev@Elshans-MacBook-Pro.local>
2026-06-03 16:34:04 +0400

    Fix CORS pattern

--- src/main/java/com/example/aliyevgrocery/config/SecurityConfig.java
+++ src/main/java/com/example/aliyevgrocery/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
 
